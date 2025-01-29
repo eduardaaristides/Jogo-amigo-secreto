28ce94974dfb211dd05d6ea4378c846868920404
maria eduarda <eduardaaristides@gmail.com>
2025-01-29 16:47:14 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,42 @@
+let amigos = [];
+
+function adicionarAmigo() {
+    let nome = document.querySelector('#amigo');
+    if (nome.value == "") {
+        alert("digite um nome");
+    } else {
+        amigos.push(nome.value);
+    }
+    limparCampo();
+    listaDeAmigos();
+    
+}
+
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value = '';
+}
+
+function listaDeAmigos() {
+    let listaAmigos = document.querySelector('#listaAmigos');
+    listaAmigos.innerHTML = ''; // Limpa a lista antes de adicionar os novos itens
+
+    for (let i = 0; i < amigos.length; i++) {
+        const item = document.createElement("li");
+        item.textContent = amigos[i];
+        listaAmigos.appendChild(item); // Adiciona o item à lista
+    }
+}
+
+function sortearAmigo(){
+    let resultado = document.querySelector("#resultado")
+    const sorteioAmigo = document.createElement("li")
+    let indiceSorteado = sorteioNome()
+    sorteioAmigo.textContent = amigos[indiceSorteado]
+    resultado.appendChild(sorteioAmigo)
+}
+
+
+function sorteioNome(){
+   return Math.floor(Math.random()* amigos.length)  
+}
